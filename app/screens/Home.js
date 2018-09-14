16ad10c4342e1c72ea219556954867dfab5fd7df
Anastasiia Stavitskaya <stavitskaya.anastasiia@gmail.com>
2018-09-13 18:53:02 -0700

    add text component with last converted info

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -5,11 +5,14 @@ import { Container } from '../components/Container';
 import { Logo } from '../components/Logo';
 import { InputWithButton } from '../components/TextInput';
 import { ClearButton } from '../components/Buttons';
+import { LastConverted } from '../components/Text';
 
 const TEMP_BASE_CURRENCY = 'USD';
 const TEMP_QUOTE_CURRENCY = 'RUB';
 const TEMP_BASE_PRICE = '100';
 const TEMP_QUOTE_PRICE = '6826.37';
+const TEMP_CONVERSION_RATE = 68.41;
+const TEMP_CONVERSION_DATE = new Date();
 
 
 class Home extends Component {
@@ -46,6 +49,12 @@ class Home extends Component {
           buttonText={TEMP_QUOTE_CURRENCY}
           editable={false}
           value={TEMP_QUOTE_PRICE}
+        />
+        <LastConverted
+          base={TEMP_BASE_CURRENCY}
+          quote={TEMP_QUOTE_CURRENCY}
+          date={TEMP_CONVERSION_DATE}
+          conversionRate={TEMP_CONVERSION_RATE}
         />
         <ClearButton
           text="Reverse Currencies"
