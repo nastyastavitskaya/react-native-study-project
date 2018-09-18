771ce1f8cc21af1cd5c2dc64318e90302745425b
Anastasiia Stavitskaya <stavitskaya.anastasiia@gmail.com>
2018-09-17 19:19:34 -0700

    add Options screen

--- app/components/List/ListItem.js
+++ app/components/List/ListItem.js
@@ -5,7 +5,9 @@ import { View, Text, TouchableHighlight } from 'react-native';
 import styles from './styles';
 import Icon from './Icon';
 
-const ListItem = ({ text, onPress, selected = false, checkmark = true, visible = true }) => (
+const ListItem = ({
+  text, onPress, selected = false, checkmark = true, visible = true, customIcon = null, 
+}) => (
   <TouchableHighlight
     onPress={onPress}
     underlayColor={styles.$underlayColor}
@@ -13,6 +15,7 @@ const ListItem = ({ text, onPress, selected = false, checkmark = true, visible =
     <View style={styles.row}>
       <Text style={styles.text}>{text}</Text>
       {selected ? <Icon checkmark={checkmark} visible={visible} /> : <Icon />}
+      {customIcon}
     </View>
   </TouchableHighlight>
 );
@@ -23,6 +26,7 @@ ListItem.propTypes = {
   selected: PropTypes.bool,
   checkmark: PropTypes.bool,
   visible: PropTypes.bool,
+  customIcon: PropTypes.element,
 };
 
 export default ListItem;
