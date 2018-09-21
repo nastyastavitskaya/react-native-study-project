0ca5fe98d580be4ffce56b4ae56108974a5b3762
Anastasiia Stavitskaya <stavitskaya.anastasiia@gmail.com>
2018-09-20 18:42:27 -0700

    Currency List Screen: Reading Currency Data from the Store

--- app/screens/CurrencyList.js
+++ app/screens/CurrencyList.js
@@ -7,12 +7,12 @@ import { ListItem, Separator } from '../components/List';
 import currencies from '../data/currencies';
 import { changeBaseCurrency, changeQuoteCurrency } from '../actions/currencies';
 
-const TEMP_CURRENT_CURRENCY = 'CAD';
-
 class CurrencyList extends Component {
   static propTypes = {
     navigation: PropTypes.object,
     dispatch: PropTypes.func,
+    baseCurrency: PropTypes.string,
+    quoteCurrency: PropTypes.string,
   }
 
   handlePress = (currency) => {
@@ -27,6 +27,12 @@ class CurrencyList extends Component {
   };
 
   render() {
+    const { baseCurrency, quoteCurrency, navigation } = this.props;
+    let comparisonCurrency = baseCurrency;
+    if (navigation.state.params.type === 'quote') {
+      comparisonCurrency = quoteCurrency;
+    }
+
     return (
       <View style={{ flex: 1 }}>
         <StatusBar barStyle="default" translucent={false} />
@@ -35,7 +41,7 @@ class CurrencyList extends Component {
           renderItem={({ item }) => (
             <ListItem
               text={item}
-              selected={item === TEMP_CURRENT_CURRENCY}
+              selected={item === comparisonCurrency}
               onPress={() => this.handlePress(item)}
             />
           )}
@@ -47,4 +53,9 @@ class CurrencyList extends Component {
   }
 }
 
-export default connect()(CurrencyList);
+const mapStateToProps = state => ({
+  baseCurrency: state.currencies.baseCurrency,
+  quoteCurrency: state.currencies.quoteCurrency,
+});
+
+export default connect(mapStateToProps)(CurrencyList);
