5b63e6f2fbcbf4138cb95770c4a4a1db09bd6d04
Anastasiia Stavitskaya <stavitskaya.anastasiia@gmail.com>
2018-09-24 18:11:04 -0700

    Handling Errors

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -9,6 +9,7 @@ import { Logo } from '../components/Logo';
 import { InputWithButton } from '../components/TextInput';
 import { ClearButton } from '../components/Buttons';
 import { LastConverted } from '../components/Text';
+import { connectAlert } from '../components/Alert';
 
 import { swapCurrency, changeCurrencyAmount, getInitialConversion } from '../actions/currencies';
 
@@ -24,11 +25,20 @@ class Home extends Component {
     isFetching: PropTypes.bool,
     lastConvertedDate: PropTypes.object,
     primaryColor: PropTypes.string,
+    alertWithType: PropTypes.func,
+    currencyError: PropTypes.string,
   };
 
   componentWillMount() {
     const { dispatch } = this.props;
     dispatch(getInitialConversion());
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { currencyError, alertWithType } = this.props;
+    if (nextProps.currencyError && nextProps.currencyError !== currencyError) {
+      alertWithType('error', 'Error', nextProps.currencyError);
+    }
   }
 
   handlePressBaseCurrency = () => {
@@ -135,7 +145,8 @@ const mapStateToProps = (state) => {
     isFetching: conversionSelector.isFetching,
     lastConvertedDate: conversionSelector.date ? new Date(conversionSelector.date) : new Date(),
     primaryColor: state.theme.primaryColor,
+    currencyError: state.currencies.error,
   };
 };
 
-export default connect(mapStateToProps)(Home);
+export default connect(mapStateToProps)(connectAlert(Home));
